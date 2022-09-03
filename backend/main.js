219ba6f03fb7d770b3d97a59bb0283cd59caeedd
ProEpicGamer1 <proepicgamer1@gmail.com>
2022-09-03 16:15:50 -0700

    changed to json from html

--- backend/main.js
+++ backend/main.js
@@ -7,7 +7,7 @@ const url = require("url");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": "text/json" });
 
     let q = url.parse(req.url, true).query;
 
