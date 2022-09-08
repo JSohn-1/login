15145bc58d6a9d52e4a6f39126ea200c3c91b84c
ProEpicGamer1 <proepicgamer1@gmail.com>
2022-09-07 21:04:55 -0700

    scheduled job

--- backend/main.js
+++ backend/main.js
@@ -4,14 +4,40 @@ const results = require('./modules/results.js');
 const time = require('./modules/time.js');
 const http = require("http");
 const url = require("url");
+const constants = require('./modules/constants.js');
+const schedule = require('node-schedule');
+
+const job = schedule.scheduleJob('0 0 * * *', () => {
+  if(database.s.length > 0){
+    database.createFile(true);
+    database.delete();
+    database.s = [];
+  }
+})
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/json" });
 
     let q = url.parse(req.url, true).query;
-
+    const url = req.url.split("?")[0];
     let success = new results(true, 40, "none");
+
+    if(url == "/gen/"){
+      success.reason = database.generate();
+
+      res.end(
+        JSON.stringify(
+          {
+            success: success.success,
+            reason: success.reason,
+            code: success.code,
+            login: q.login
+          },
+        )
+      );
+    }
+
 
     if(q.name == undefined){
         success = new results(false, 10, "No name given");
@@ -27,14 +53,14 @@ http
 
     if(success.success === true){
       const st = new student(q.id, q.name, q.grade, time.getTime());
-      const url = req.url.split("?")[0];
+
 
       if(url === "/entry/"){
         success = database.enter(st);
       }else if (url === "/exit/"){
         success = database.exit(st);
       }else if (url === "/flush/"){
-        success = database.createFile();
+        success = database.createFile(q.write);
         database.s = [];
       }
     }
--- backend/modules/database.js
+++ backend/modules/database.js
@@ -7,7 +7,8 @@ const path = require('path');
 
 class database{
     static s = [];
-   
+    static last = "";
+
     static enter(student){
         let stu = student;
         let r = new results();
@@ -49,7 +50,7 @@ class database{
         return(r);    
     }
    
-    static  createFile(){
+    static createFile(write){
         let output = [["name", "id", "grade", "entry time", "exit time"],];
         let r = new results();
         
@@ -64,20 +65,55 @@ class database{
             oStr += output[i].join() + "\n";
         }
 
-         fs.writeFile(path.join(constants.FILESTORAGE, constants.CLUB + " - " + time.getDate() + ".csv"), oStr, (err) =>{
-            if(err){
-                r.success = false;
-                r.reason = err;
-                r.code = 1;
-            }else{
-                console.log("success");
-            }
-        });
-        //})
+        if(write != null){
+            fs.writeFile(path.join(constants.FILESTORAGE, constants.CLUB + " - " + time.getDate() + ".csv"), oStr, (err) =>{
+                if(err){
+                    r.success = false;
+                    r.reason = err;
+                    r.code = 1;
+                }else{
+                    console.log("success");
+                }
+            });
+        }
         
         r.reason = oStr;
 
         return r;
+    }
+
+    static delete(){
+        if(last == ""){
+            return(new results(false, 10, "No file written"))
+        }
+
+        fs.unlink(path.join(constants.FILESTORAGE, this.last), (err) =>{
+            if(err){
+                return(new results(false, 1, err));
+            }else{
+                console.log("success");
+            }
+        });
+
+        return(new results());
+    }
+
+    static generate(){
+        let output = [["name", "id", "grade", "entry time", "exit time"],];
+        let r = new results();
+        
+        for(let i = 0; i < this.s.length; i++){
+            this.s[i].fix();
+            output.push(this.s[i].toArr());
+        }
+       
+        let oStr = "";
+
+        for(let i = 0; i < output.length; i++){
+            oStr += output[i].join() + "\n";
+        }
+
+        return(oStr);
     }
 }
 
